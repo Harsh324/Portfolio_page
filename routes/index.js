4fbae873189f46afcddcdf6ed14478143bdbfa5a
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-04-22 12:39:32 +0530

    inserting and fetching data from database done

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,60 @@
 var express = require('express');
+const { Double } = require('mongodb');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017";
 
-const data = [
-  {
-    name: "Harsh",
-    post: "Assistant Professor"
-  },
-  {
-    name: "Tripathi",
-    post: "Hole Manager"
-  },
-  {
-    name: "Giri",
-    post: "Ch**t chatora"
-  }
-]
+// const data = [
+//   {
+//     name: "Harsh",
+//     post: "Assistant Professor"
+//   },
+//   {
+//     name: "Tripathi",
+//     post: "Hole Manager"
+//   },
+//   {
+//     name: "Giri",
+//     post: "Ch**t chatora"
+//   }
+// ]
 
+var data;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {data});
+  MongoClient.connect(url, (err, db) => {
+    if(err){
+      console.log(err);
+      return;
+    }
+    var dbo = db.db("ITW");
+    dbo.collection('pfCardInfo').find({}).toArray(function (err, data){
+      if(err) throw err;
+      console.log(data);
+      res.render('index', {data});
+    })
+  });
 });
+
+router.get('/pfCardForm', function(req, res, next) {
+  res.render('pfCardForm', {});
+});
+
+router.post('/saveData', function (req, res) {
+  MongoClient.connect(url, (err, db) => {
+    if(err){
+      console.log(err);
+      return;
+    }
+    var dbo = db.db("ITW");
+    dbo.collection("pfCardInfo").insertOne(req.body, (err, res) => {
+      if(err){
+        console.log(err);
+      }
+    })
+  });
+  res.redirect('/');
+});
+
 
 module.exports = router;
