7814e7ca0b5047851474c180e314e4cd970725dd
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-04-22 11:28:44 +0530

    first success

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const data = [
+  {
+    name: "Harsh",
+    post: "Assistant Professor"
+  },
+  {
+    name: "Tripathi",
+    post: "Hole Manager"
+  },
+  {
+    name: "Giri",
+    post: "Ch**t chatora"
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {data});
 });
 
 module.exports = router;
