dbaa36245a8a784b9a249ceda5b45e2e41b81edb
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-04-23 10:37:54 +0530

    done

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.get('/', function(req, res, next) {
     var dbo = db.db("ITW");
     dbo.collection('pfCardInfo').find({}).toArray(function (err, data){
       if(err) throw err;
-      console.log(data);
+      // console.log(data);
       res.render('index', {data});
     })
   });
@@ -44,8 +44,31 @@ router.get('/pfPortfolio', function(req, res, next) {
   res.render('pfPortfolio', {});
 });
 
-router.get('/ppPage', function(req, res, next) {
-  res.render('ppPage', {});
+router.get('/ppPage', function(req, result, next) {
+  // console.log(req.query.id)
+  MongoClient.connect(url, (err, db) => {
+    if(err){
+      console.log(err);
+      return;
+    }
+    // console.log(req.body.name);?
+    var dbo = db.db("ITW");
+    dbo.collection('pfCardInfo').find({name: req.query.name, post: req.query.post}).toArray((err, res) => {
+      if(err) throw err;
+      res[0].education = res[0].education.split(';');
+      res[0].publication = res[0].publication.split(';');
+      res[0].supervision = res[0].supervision.split(';');
+      res[0].responsibility = res[0].responsibility.split(';');
+      res[0].experience = res[0].experience.split(';');
+      res[0].currently_teaching = res[0].currently_teaching.split(';');
+      res[0].research = res[0].research.split(';');
+      console.log(res);
+      
+      result.render('ppPage', {res});
+      db.close();
+    });
+    // console.log("here" + x);
+  });
 });
 
 router.post('/saveData', function (req, res) {
@@ -55,6 +78,7 @@ router.post('/saveData', function (req, res) {
       return;
     }
     var dbo = db.db("ITW");
+    // console.log(req.body);
     dbo.collection("pfCardInfo").insertOne(req.body, (err, res) => {
       if(err){
         console.log(err);
